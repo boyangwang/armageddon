a6cbb4d6dbe69e2a6cbc385e7b8a652bac3e8b02
Wang Boyang <boyang@dae.org>
2019-04-28 20:03:37 +0800

    Add server

---
+++ server/src/main/java/com/armageddon/armageddonserver/ArmageddonServerApplication.java
@@ -0,0 +1,13 @@
+package com.armageddon.armageddonserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmageddonServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmageddonServerApplication.class, args);
+	}
+
+}
